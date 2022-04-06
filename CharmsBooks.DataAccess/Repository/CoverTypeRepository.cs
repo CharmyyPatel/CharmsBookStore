50b5fa5e71378c3f766ff99f9fdb05a35bcdc17f
CharmyyPatel <90396296+CharmyyPatel@users.noreply.github.com>
2022-04-06 12:20:21 -0400

    Part 3

---
+++ CharmsBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,32 @@
+﻿using CharmsBooks.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharmsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public static object Id { get; private set; }
+
+        public void Update(Models.Category category)
+        {
+            // use .Net Linq to retrieve the first or default category object,
+            // then pass the id as a generic entity which matches the category Id
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == CoverTypeRepository.Id);
+            if (objFromDb != null) // save changes if not null
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CharmsBooks.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharmsBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository: Repository<CoverTypeRepository>
+    {
+        void Update(CoverTypeRepository coverTypeRepository);
+    }
+}
